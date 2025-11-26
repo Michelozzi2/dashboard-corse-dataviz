6636a26294110c370c01eac8aa907fd75e98d692
Michelozzi2 <michelozzimatthieu@gmail.com>
2025-11-26 22:06:19 +0100

    Correction base path vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dashboard-corse/', 
+  // ATTENTION : Doit être le nom EXACT de ton dépôt GitHub entre deux slashs
+  base: '/dashboard-corse-dataviz/', 
 })
